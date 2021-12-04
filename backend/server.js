e4ff96c63d93a289d8558f72d6ffd0530209165e
Briana King <brianaking1330@gmail.com>
2021-12-04 13:49:04 -0700

    Fixing server

--- backend/server.js
+++ backend/server.js
@@ -96,4 +96,4 @@ app.delete('/api/animals/:id', async (req, res) => {
 });
 
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3003, () => console.log('Server listening on port 3003!'));
